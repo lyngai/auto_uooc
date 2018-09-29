87cd6174d67f603d27b00cb35be92ea24e7ac879
lyngai <wlxjust@gmail.com>
2018-09-29 11:17:38 +0800

    init repo

---
+++ background.js
@@ -0,0 +1,17 @@
+chrome.runtime.onInstalled.addListener(function(){
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          // 只有打开百度才显示pageAction
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+              urlContains: 'uooc.net.cn'
+            }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+});
